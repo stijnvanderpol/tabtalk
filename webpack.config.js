6d549dbd60331544f322cc5ae20d933acdbadd37
Stijn van der Pol <s.o.vanderpol@gmail.com>
2019-12-18 12:10:03 +0100

    configure webpack and jest to run with babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         rules: [
             {
                 test: /\.ts?$/,
-                use: 'ts-loader',
+                use: 'babel-loader',
                 exclude: /node_modules/
             }
         ]
