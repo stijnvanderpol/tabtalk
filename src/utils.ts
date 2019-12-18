7b2965c5ee189e0966ea8617e59a9b8e5c774b8e
Stijn van der Pol <s.o.vanderpol@gmail.com>
2019-12-18 09:38:11 +0100

    add a utility function to generate a random integer value and a utility function to generate a random letter

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,20 @@ export const throwIfSessionStorageUnavailable = () => {
         throw new Error('[tabtalk] session storage is not available.')
     }
 }
+
+export const generateUuid = () => {
+    const now = Date.now();
+}
+
+/**
+ * Returns a random letter ranging from a to z.
+ * @param isUpperCase (default true) Determines whether the random letter should be upper case.
+ */
+export const getRandomLetter = (isUpperCase = true) => {
+    const letter = String.fromCharCode(97 + getRandomInteger(25));
+    return isUpperCase ? letter.toUpperCase() : letter;
+}
+
+export const getRandomInteger = (maxInteger: number) => {
+    return Math.round(Math.random() * Math.floor(maxInteger));
+}
