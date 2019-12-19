7633a7c306a1c8c3b6dbef1182d3d8edc5e4d154
Stijn van der Pol <s.o.vanderpol@gmail.com>
2019-12-19 14:51:37 +0100

    add an event listener that dispatches a custom tabtalk message event to the window when receiving a storage event with a keyname prefixed with the tabtalk prefix

--- src/tabtalk.ts
+++ src/tabtalk.ts
@@ -3,13 +3,17 @@ import { throwIfLocalStorageUnavailable, generateUuid } from './utils';
 export class Tabtalk {
     private id: string;
     private TABTALK_WINDOW_OBJECT_PROPERTY_KEY = 'tabtalk';
+    private TABTALK_MESSAGE_KEY_PREFIX = 'TABTALK-';
+    private TABTALK_MESSAGE_EVENT_NAME = 'TABTALK_MESSAGE_RECEIVED';
     
     init() {
         throwIfLocalStorageUnavailable();
         this.id = generateUuid();
         
         // @ts-ignore implicit any is acceptable here
-        window[this.TABTALK_WINDOW_OBJECT_PROPERTY_KEY] = this;        
+        window[this.TABTALK_WINDOW_OBJECT_PROPERTY_KEY] = this;   
+        
+        window.addEventListener('storage', this.onStorageUpdate);
     }
 
     destroy() {
@@ -18,4 +22,14 @@ export class Tabtalk {
     }
 
     getId = () => this.id;
+
+    private onStorageUpdate = (event: StorageEvent) => {
+        const { key, newValue: value } = event;
+        const isTabtalkMessage = key.match(new RegExp(`^${this.TABTALK_MESSAGE_KEY_PREFIX}`));
+
+        if (isTabtalkMessage) {
+            const event = new CustomEvent(this.TABTALK_MESSAGE_EVENT_NAME, { detail: value });
+            window.dispatchEvent(event);    
+        }
+    }
 }
