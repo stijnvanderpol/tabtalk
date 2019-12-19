3cfd8692631d264d9eb6460d291125352b0f8973
Stijn van der Pol <s.o.vanderpol@gmail.com>
2019-12-19 12:20:50 +0100

    add a reference to tabtalk to the window object on init and add a method to remove it

--- src/tabtalk.spec.ts
+++ src/tabtalk.spec.ts
@@ -2,22 +2,40 @@ import { Tabtalk } from './tabtalk';
 
 describe('Tabtalk', () => {
     describe('init', () => {
-    
+        let tabtalk: Tabtalk;
+        
+        afterEach(() => {
+            if (tabtalk) {
+                tabtalk.destroy();
+            }
+        });
+
         // todo: add a test that verifies the app throws an error on unavailable session storage
 
         it('does not throw an error if the session storage api is available', () => {
-            const tabtalk = new Tabtalk();
+            tabtalk = new Tabtalk();
             expect(() => tabtalk.init()).not.toThrow();
         });
 
         it('sets a uuid after being initialized', () => {
-            const tabtalk = new Tabtalk();
+            tabtalk = new Tabtalk();
 
             expect(tabtalk.getId()).toBeUndefined();
             tabtalk.init();
 
             expect(tabtalk.getId()).not.toBeUndefined();
             expect(typeof tabtalk.getId()).toEqual('string');
+        });
+
+        it('adds the tabtalk instance as a property to the window object after initializing', () => {
+            tabtalk = new Tabtalk();
+            
+            // @ts-ignore implicit any type is acceptable here
+            expect(window[tabtalk.TABTALK_WINDOW_OBJECT_PROPERTY_KEY]).toBeUndefined();
+
+            tabtalk.init();            
+            // @ts-ignore implicit any type is acceptable here
+            expect(window[tabtalk.TABTALK_WINDOW_OBJECT_PROPERTY_KEY]).toStrictEqual(tabtalk);
         });
     });
 });
--- src/tabtalk.ts
+++ src/tabtalk.ts
@@ -2,10 +2,19 @@ import { throwIfSessionStorageUnavailable, generateUuid } from './utils';
 
 export class Tabtalk {
     private id: string;
+    private TABTALK_WINDOW_OBJECT_PROPERTY_KEY = 'tabtalk';
     
     init() {
         throwIfSessionStorageUnavailable();
         this.id = generateUuid();
+        
+        // @ts-ignore implicit any is acceptable here
+        window[this.TABTALK_WINDOW_OBJECT_PROPERTY_KEY] = this;
+    }
+
+    destroy() {
+        // @ts-ignore implicit any type is acceptable in this situation
+        delete window[this.TABTALK_WINDOW_OBJECT_PROPERTY_KEY];
     }
 
     getId = () => this.id;
