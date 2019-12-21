91f12b1906a451df5f294b9d9c59aaecb5fb39ad
Stijn van der Pol <s.o.vanderpol@gmail.com>
2019-12-21 22:37:33 +0100

    remove the logic that adds tabtalk to the window object

--- src/tabtalk.spec.ts
+++ src/tabtalk.spec.ts
@@ -2,40 +2,22 @@ import { Tabtalk } from './tabtalk';
 
 describe('Tabtalk', () => {
     describe('init', () => {
-        let tabtalk: Tabtalk;
-        
-        afterEach(() => {
-            if (tabtalk) {
-                tabtalk.destroy();
-            }
-        });
 
         // todo: add a test that verifies the app throws an error on unavailable session storage
 
         it('does not throw an error if the session storage api is available', () => {
-            tabtalk = new Tabtalk();
+            const tabtalk = new Tabtalk();
             expect(() => tabtalk.init()).not.toThrow();
         });
 
         it('sets a uuid after being initialized', () => {
-            tabtalk = new Tabtalk();
+            const tabtalk = new Tabtalk();
 
             expect(tabtalk.getId()).toBeUndefined();
             tabtalk.init();
 
             expect(tabtalk.getId()).not.toBeUndefined();
             expect(typeof tabtalk.getId()).toEqual('string');
-        });
-
-        it('adds the tabtalk instance as a property to the window object after initializing', () => {
-            tabtalk = new Tabtalk();
-            
-            // @ts-ignore implicit any type is acceptable here
-            expect(window[tabtalk.TABTALK_WINDOW_OBJECT_PROPERTY_KEY]).toBeUndefined();
-
-            tabtalk.init();            
-            // @ts-ignore implicit any type is acceptable here
-            expect(window[tabtalk.TABTALK_WINDOW_OBJECT_PROPERTY_KEY]).toStrictEqual(tabtalk);
         });
     });
 });
--- src/tabtalk.ts
+++ src/tabtalk.ts
@@ -16,16 +16,7 @@ export class Tabtalk {
     init() {
         throwIfLocalStorageUnavailable();
         this.id = generateUuid();
-        
-        // @ts-ignore implicit any is acceptable here
-        window[this.TABTALK_WINDOW_OBJECT_PROPERTY_KEY] = this;   
-        
         window.addEventListener('storage', this.onStorageUpdate);
-    }
-
-    destroy() {
-        // @ts-ignore implicit any type is acceptable in this situation
-        delete window[this.TABTALK_WINDOW_OBJECT_PROPERTY_KEY];
     }
 
     getId = () => this.id;
